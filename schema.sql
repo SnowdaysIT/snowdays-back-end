bd6a2f6188d505689fee911a7c525299b012c917
riccardofelluga <riki.1@hotmail.it>
2019-12-21 17:02:12 +0100

    added n-to-m relation  profile_activity

--- schema.sql
+++ schema.sql
@@ -6,8 +6,9 @@ CREATE TYPE gender AS ENUM ('Female', 'Male');
 CREATE TYPE order_status AS ENUM ('Submitted', 'Confirmed', 'Delivered');
 CREATE TABLE public_api.activity (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name TEXT CHECK (CHAR_LENGTH(name) < 50),
-  date DATE
+  name TEXT NOT NULL CHECK (CHAR_LENGTH(name) < 50),
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL
 );
 CREATE TABLE public_api.profile (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -15,23 +16,15 @@ CREATE TABLE public_api.profile (
   last_name TEXT NOT NULL CHECK (CHAR_LENGTH(last_name) < 80),
   mobile_phone TEXT NOT NULL CHECK (CHAR_LENGTH(mobile_phone) < 20),
   badge_number TEXT NOT NULL CHECK (CHAR_LENGTH(badge_number) < 20),
-  gender gender,
-  vegetarian boolean,
+  gender gender NOT NULL,
+  vegetarian BOOLEAN NOT NULL,
   height INTEGER,
   weight INTEGER,
-  shoe_size INTEGER,
-  activity UUID REFERENCES public_api.activity(id)
+  shoe_size INTEGER
 );
-CREATE TABLE public_api.university (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  contact_person UUID REFERENCES public_api.profile(id),
-  address TEXT CHECK (CHAR_LENGTH(address) < 100),
-  name TEXT NOT NULL CHECK (CHAR_LENGTH(name) < 100)
-);
-CREATE TABLE public_api.accommodation (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  address TEXT NOT NULL CHECK (CHAR_LENGTH(address) < 50),
-  host_id UUID REFERENCES public_api.profile(id)
+CREATE TABLE public_api.profile_activity (
+  profile_id UUID REFERENCES public_api.profile(id),
+  activity_id UUID REFERENCES public_api.activity(id)
 );
 CREATE TABLE public_api.address (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -40,29 +33,39 @@ CREATE TABLE public_api.address (
   city TEXT NOT NULL CHECK (CHAR_LENGTH(street) < 30),
   country TEXT NOT NULL CHECK (CHAR_LENGTH(street) < 15)
 );
-CREATE TABLE public_api.order (
+CREATE TABLE public_api.university (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  total INTEGER,
-  status order_status
+  contact_person UUID REFERENCES public_api.profile(id),
+  address UUID NOT NULL REFERENCES public_api.address(id),
+  name TEXT NOT NULL CHECK (CHAR_LENGTH(name) < 100)
 );
-CREATE TABLE public_api.merchandising (
+CREATE TABLE public_api.accommodation (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  address UUID NOT NULL REFERENCES public_api.address(id),
+  host_id UUID REFERENCES public_api.profile(id)
+);
+CREATE TABLE public_api.purchase (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  total_price INTEGER NOT NULL,
+  status order_status NOT NULL
+);
+CREATE TABLE public_api.item (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL CHECK (CHAR_LENGTH(name) < 20),
   description TEXT NOT NULL CHECK (CHAR_LENGTH(description) < 100),
-  available TEXT NOT NULL CHECK (CHAR_LENGTH(available) < 30),
-  price INTEGER
+  available INTEGER NOT NULL,
+  price NUMERIC(6,2) NOT NULL
 );
-CREATE TABLE public_api.order_merch (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  order_id UUID REFERENCES public_api.order(id),
-  item_id UUID REFERENCES public_api.merchandising(id),
-  quantity INTEGER
+CREATE TABLE public_api.purchase_item (
+  purchase_id UUID REFERENCES public_api.purchase(id),
+  item_id UUID REFERENCES public_api.item(id),
+  quantity INTEGER NOT NULL
 );
-CREATE TABLE private_api.user (
+CREATE TABLE private_api.account (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   profile_id UUID REFERENCES public_api.profile(id),
   email TEXT NOT NULL UNIQUE CHECK (email ~* '^.+@.+\..+$'),
-  password TEXT NOT NULL CHECK (CHAR_LENGTH(password) < 80)
+  password TEXT NOT NULL
 );
 ALTER TABLE public_api.profile
 ADD
@@ -70,5 +73,5 @@ ADD
 ADD
   CONSTRAINT accommodation_id FOREIGN KEY (id) REFERENCES public_api.accommodation(id),
 ADD
-  CONSTRAINT order_id FOREIGN KEY (id) REFERENCES public_api.order(id);
+  CONSTRAINT purchase_id FOREIGN KEY (id) REFERENCES public_api.purchase(id);
 COMMIT;
