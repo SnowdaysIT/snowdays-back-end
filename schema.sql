d7e28f756e1416736808d7a1ed8282b6ad39d9af
riccardofelluga <riki.1@hotmail.it>
2019-12-23 13:53:19 +0100

    Added rentals and updated profile

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ CREATE SCHEMA public_api;
 CREATE SCHEMA private_api;
 CREATE TYPE gender AS ENUM ('Female', 'Male');
 CREATE TYPE order_status AS ENUM ('Submitted', 'Confirmed', 'Delivered');
+CREATE TYPE experience_level AS ENUM('Beginner', 'Intermediate');
 CREATE TABLE public_api.activity (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL CHECK (CHAR_LENGTH(name) < 50),
@@ -17,17 +18,34 @@ CREATE TABLE public_api.profile (
   mobile_phone TEXT NOT NULL CHECK (CHAR_LENGTH(mobile_phone) < 20),
   badge_number TEXT NOT NULL CHECK (CHAR_LENGTH(badge_number) < 20),
   gender gender NOT NULL,
-  vegetarian BOOLEAN NOT NULL,
-  height INTEGER,
-  weight INTEGER,
-  shoe_size INTEGER,
+  is_vegetarian BOOLEAN NOT NULL,
+  is_helper BOOLEAN NOT NULL,
+  id_card TEXT NOT NULL,
+  rental_id UUID,
   university_id UUID,
   accommodation_id UUID,
   purchase_id UUID
 );
 CREATE TABLE public_api.profile_activity (
-  profile_id UUID REFERENCES public_api.profile(id),
-  activity_id UUID REFERENCES public_api.activity(id)
+  profile_id UUID REFERENCES public_api.profile(id) ON DELETE CASCADE,
+  activity_id UUID REFERENCES public_api.activity(id) ON DELETE RESTRICT,
+  PRIMARY KEY (profile_id, activity_id)
+);
+CREATE TABLE public_api.material(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT
+);
+CREATE TABLE public_api.rental (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  height INTEGER,
+  weight INTEGER,
+  shoe_size INTEGER,
+  experience experience_level NOT NULL
+);
+CREATE TABLE public_api.rental_material (
+  rental_id UUID REFERENCES public_api.rental(id) ON DELETE CASCADE,
+  material_id UUID REFERENCES public_api.material(id) ON DELETE RESTRICT,
+  PRIMARY KEY (rental_id, material_id)
 );
 CREATE TABLE public_api.address (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -45,24 +63,27 @@ CREATE TABLE public_api.university (
 CREATE TABLE public_api.accommodation (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   address UUID NOT NULL REFERENCES public_api.address(id),
+  is_dormroom BOOLEAN NOT NULL,
+  places NUMERIC(4, 0) NOT NULL,
   host_id UUID REFERENCES public_api.profile(id)
 );
 CREATE TABLE public_api.purchase (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  total_price INTEGER NOT NULL,
   status order_status NOT NULL
 );
 CREATE TABLE public_api.item (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL CHECK (CHAR_LENGTH(name) < 20),
   description TEXT NOT NULL CHECK (CHAR_LENGTH(description) < 100),
-  available INTEGER NOT NULL,
-  price NUMERIC(6, 2) NOT NULL
+  available NUMERIC(4, 0) NOT NULL,
+  price NUMERIC(6, 2) NOT NULL,
+  image_link TEXT
 );
 CREATE TABLE public_api.purchase_item (
-  purchase_id UUID REFERENCES public_api.purchase(id),
-  item_id UUID REFERENCES public_api.item(id),
-  quantity INTEGER NOT NULL
+  purchase_id UUID REFERENCES public_api.purchase(id) ON DELETE CASCADE,
+  item_id UUID REFERENCES public_api.item(id) ON DELETE RESTRICT,
+  quantity INTEGER NOT NULL,
+  PRIMARY KEY (purchase_id, item_id)
 );
 CREATE TABLE private_api.account (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -76,5 +97,7 @@ ADD
 ADD
   FOREIGN KEY (accommodation_id) REFERENCES public_api.accommodation(id),
 ADD
-  FOREIGN KEY (purchase_id) REFERENCES public_api.purchase(id);
+  FOREIGN KEY (purchase_id) REFERENCES public_api.purchase(id),
+ADD 
+  FOREIGN KEY (rental_id) REFERENCES public_api.rental(id);
 COMMIT;
--- template.seed.sql
+++ template.seed.sql
@@ -3,7 +3,7 @@ BEGIN;
 INSERT INTO private_api.account (email, password) values
     ('testmail@snowdays.it', 'strong-hash-psw');
 
-INSERT INTO public_api.profile (first_name, last_name, mobile_phone, badge_number, gender, vegetarian) values
-    ('Yeti', 'The Mascott Master', '0000000000', '12345A', 'Male', false);
+INSERT INTO public_api.profile (first_name, last_name, mobile_phone, badge_number, gender, is_vegetarian, is_helper, id_card) values
+    ('Yeti', 'The Mascott Master', '0000000000', '12345A', 'Male', false, false, 'card_id here');
 
 COMMIT;
