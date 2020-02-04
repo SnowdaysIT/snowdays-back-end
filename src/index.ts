fb716db397f2b184fee6ae45b8fd3c4a90c03d79
riccardofelluga <riki.1@hotmail.it>
2020-02-04 23:20:37 +0100

    disabled upload

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
 import PgSimplifyInflector from '@graphile-contrib/pg-simplify-inflector'
 import express from 'express'
 import { graphqlUploadExpress } from 'graphql-upload'
-import path from 'path'
 import { postgraphile } from 'postgraphile'
-import PostGraphileUploadFieldPlugin from 'postgraphile-plugin-upload-field'
-import Upload from './upload'
 // Create a new express application instance
 const app: express.Application = express()
 
-app.use(`/${process.env.UPLOAD_DIR_NAME || 'upload'}`, express.static(path.resolve(process.env.UPLOAD_DIR_NAME || 'upload')))
-
 const postgraphileOptions: object = {
-    appendPlugins: [PgSimplifyInflector, PostGraphileUploadFieldPlugin],
+    appendPlugins: [PgSimplifyInflector],
     dynamicJson: true,
     enableCors: true,
     enableQueryBatching: true,
     enhanceGraphiql: true,
     exportGqlSchemaPath: 'schema.graphql',
     extendedErrors: ['hint', 'detail', 'errcode'],
-    graphileBuildOptions: {
-        uploadFieldDefinitions: [
-            {
-                match: ({ schema, table, column, tags }: any) =>
-                    (column === 'badge_front_id' || column === 'badge_back_id'),
-                resolve: Upload.resolve,
-            },
-        ],
-    },
-    graphiql: true,
+    graphileBuildOptions: {},
+    graphiql: process.env.ENABLE_PLAYGROUND || false,
     ignoreIndexes: false,
     ignoreRBAC: false,
     jwtPgTypeIdentifier: 'public_api.jwt_token',
